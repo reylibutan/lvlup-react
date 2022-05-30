28d9e44bcfb6664cc8b7b31fa5d45c3c6809414b
Reynaldo Libutan <reynaldo_libutan@epam.com>
2022-05-30 15:56:36 +0800

    reorganize components folder structure

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,4 +1,4 @@
-import Card from "./Card";
+import Card from "../Common/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -1,4 +1,4 @@
-import Card from "./Card";
+import Card from "../Common/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
