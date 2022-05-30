76115b24c4d5df8d5ff380e2d55e79228e2a3922
Reynaldo Libutan <reynaldo_libutan@epam.com>
2022-05-30 14:13:30 +0800

    initial commit for Expense component checking

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
+
+function App() {
+  const expenses = [
+    {
+      title: "Food and Transportation",
+      amount: 111.11,
+      date: new Date(2022, 1, 1),
+    },
+    { title: "Income Tax", amount: 222.22, date: new Date(2022, 2, 2) },
+    { title: "Apartment Rental", amount: 333.33, date: new Date(2022, 3, 3) },
+    {
+      title: "Mandatory Provident Fund",
+      amount: 444.44,
+      date: new Date(2022, 4, 4),
+    },
+  ];
+
+  return (
+    <div>
+      <h2>Let's get started! 222</h2>
+      <br />
+      <Expenses expenses={expenses} />
+    </div>
+  );
+}
+
+export default App;
