ad544c521e2973aa90a817fa112b55ccc60a3bb2
Rey Libutan <reylibutan@gmail.com>
2022-06-01 18:15:27 +0800

    style-components vs CSS modules

--- 06-xx - goals/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 06-xx - goals/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,26 +1,64 @@
 import React, { useState } from "react";
-
+import styled from "styled-components";
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+
+const FormControl = styled.div`
+  & {
+    margin: 0.5rem 0;
+  }
+
+  & label {
+    color: ${(props) => (props.invalid ? "red" : "black")};
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+    background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+`;
 
 const CourseInput = (props) => {
+  const [isValidInput, setIsValidInput] = useState(true);
   const [enteredValue, setEnteredValue] = useState("");
 
   const goalInputChangeHandler = (event) => {
+    if (enteredValue.trim().length > 0) {
+      setIsValidInput(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+
+    if (enteredValue.trim().length === 0) {
+      setIsValidInput(false);
+      return;
+    }
+
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <FormControl invalid={!isValidInput}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
