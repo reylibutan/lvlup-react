667a1ced9ab49247d2d10d1979d7d9096c2a3535
Rey Libutan <reylibutan@gmail.com>
2022-06-02 16:03:49 +0800

    add backdrop

--- 08-08 - users/src/components/Common/Modal/Modal.js
+++ 08-08 - users/src/components/Common/Modal/Modal.js
@@ -3,18 +3,26 @@ import styles from "./Modal.module.css";
 
 const Modal = (props) => {
   return (
-    <div className={styles.modal}>
-      <div className={styles.header}>{props.header}</div>
-      <div className={styles.body}>{props.body}</div>
-      <div className={styles.actions}>
-        <button
-          type="button"
-          onClick={() => {
-            props.onOkayHandler();
-          }}
-        >
-          Okay
-        </button>
+    <div>
+      <div
+        className={styles.backdrop}
+        onClick={() => {
+          props.onOkayHandler();
+        }}
+      ></div>
+      <div className={styles.modal}>
+        <div className={styles.header}>{props.header}</div>
+        <div className={styles.body}>{props.body}</div>
+        <div className={styles.actions}>
+          <button
+            type="button"
+            onClick={() => {
+              props.onOkayHandler();
+            }}
+          >
+            Okay
+          </button>
+        </div>
       </div>
     </div>
   );
