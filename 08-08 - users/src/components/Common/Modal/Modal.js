37c5764aa082844e116545360b56e3dd97086a31
Rey Libutan <reylibutan@gmail.com>
2022-06-02 17:13:05 +0800

    use portal without breaking down Modal component

--- 08-08 - users/src/components/Common/Modal/Modal.js
+++ 08-08 - users/src/components/Common/Modal/Modal.js
@@ -2,46 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 import styles from "./Modal.module.css";
 
-const Backdrop = (props) => {
-  return <div className={styles.backdrop} onClick={props.onClick}></div>;
-};
-
-const BaseModal = (props) => {
-  return (
-    <div className={styles.modal}>
-      <div className={styles.header}>{props.header}</div>
-      <div className={styles.body}>{props.body}</div>
-      <div className={styles.actions}>
-        <button type="button" onClick={props.onOkayHandler}>
-          Okay
-        </button>
-      </div>
-    </div>
-  );
-};
-
 const Modal = (props) => {
-  return (
+  return ReactDOM.createPortal(
     <>
-      {ReactDOM.createPortal(
-        <Backdrop
-          onClick={() => {
-            props.onOkayHandler();
-          }}
-        />,
-        document.getElementById("backdrop-root")
-      )}
-      {ReactDOM.createPortal(
-        <BaseModal
-          header={props.header}
-          body={props.body}
-          onOkayHandler={() => {
-            props.onOkayHandler();
-          }}
-        />,
-        document.getElementById("modal-root")
-      )}
-    </>
+      <div
+        className={styles.backdrop}
+        onClick={() => {
+          props.onOkayHandler();
+        }}
+      ></div>
+      <div className={styles.modal}>
+        <div className={styles.header}>{props.header}</div>
+        <div className={styles.body}>{props.body}</div>
+        <div className={styles.actions}>
+          <button
+            type="button"
+            onClick={() => {
+              props.onOkayHandler();
+            }}
+          >
+            Okay
+          </button>
+        </div>
+      </div>
+    </>,
+    document.getElementById("modal-root")
   );
 };
 
